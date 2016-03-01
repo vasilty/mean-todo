f27ec48a409e970cac4084c4417f098005abfcf7
James Churchill <james@smashdev.com>
2016-03-01 15:53:16 -0800

    Setup Express API

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ var app = express();
 
 app.use('/', express.static('public'));
 
+var router = express.Router();
+
+router.get('/todos', function(req, res) {
+	res.json({ todos: [] });
+});
+
+// TODO: Add POST route to create new entries
+
+// TODO: Add PUT route to update existing entries
+
+// TODO: Add DELETE route to remove existing entries
+
+app.use('/api', router);
+
 app.listen(3000, function() {
     console.log("The server is running on port 3000!");
 });
