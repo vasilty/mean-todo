446b76e559139fb1f7089ec944cf0cc635e736e3
James Churchill <james@smashdev.com>
2016-03-01 16:20:31 -0800

    Added mock data

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var express = require('express');
+var todos = require('../../mock/todos.json');
 
 var router = express.Router();
 
 router.get('/todos', function(req, res) {
-	res.json({ todos: [] });
+	res.json({ todos: todos });
 });
 
 // TODO: Add POST route to create new entries
