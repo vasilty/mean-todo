6dd99cc8adf2f0c284020c22bbe8634d4639653c
James Churchill <james@smashdev.com>
2016-03-01 18:00:18 -0800

    Added POST route for todos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var parser = require('body-parser');
 var router = require('./api');
 
 var app = express();
@@ -9,6 +10,7 @@ require('./database');
 require('./seed');
 
 app.use('/', express.static('public'));
+app.use(parser.json());
 
 app.use('/api', router);
 
