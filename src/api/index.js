94b07a25cfd8149f97274a5fcda5d68ca36f8a26
James Churchill <james@smashdev.com>
2016-03-01 18:13:59 -0800

    Added PUT route for todos

--- src/api/index.js
+++ src/api/index.js
@@ -24,7 +24,19 @@ router.post('/todos', function(req, res) {
   });
 });
 
-// TODO: Add PUT route to update existing entries
+router.put('/todos/:id', function(req, res) {
+  var id = req.params.id;
+  var todo = req.body;
+  if (todo && todo._id !== id) {
+    return res.status(500).json({ err: "Ids don't match!" });
+  }
+  Todo.findByIdAndUpdate(id, todo, {new: true}, function(err, todo) {
+    if (err) {
+      return res.status(500).json({ err: err.message });
+    }
+    res.json({ 'todo': todo, message: 'Todo Updated' });
+  });
+});
 
 // TODO: Add DELETE route to remove existing entries
 
