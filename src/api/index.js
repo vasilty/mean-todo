c6721e53d3135302988f78ab4870d092490c8d8a
James Churchill <james@smashdev.com>
2016-03-01 17:36:26 -0800

    Connected model to /todos route

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 var express = require('express');
-var todos = require('../../mock/todos.json');
+var Todo = require('../models/todo');
 
 var router = express.Router();
 
 router.get('/todos', function(req, res) {
-	res.json({ todos: todos });
+  Todo.find({}, function(err, todos) {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    res.json({ todos: todos });
+  });
 });
 
 // TODO: Add POST route to create new entries
